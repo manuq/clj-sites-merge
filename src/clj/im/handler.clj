14e2aa979ca7d585672ccb0193ef912bd400be6f
Manuel Quiñones <manuel.por.aca@gmail.com>
2017-08-03 14:50:49 -0300

    one server and handler, template pages

--- src/clj/im/handler.clj
+++ src/clj/im/handler.clj
@@ -6,7 +6,7 @@
                                               site-defaults]]
             [hiccup.page :refer [include-js include-css html5]]))
 
-(def main-page
+(def im-page
   (html5
    [:head
     [:meta {:charset "utf-8"}]
@@ -18,12 +18,48 @@
     (include-js "/js/compiled/im.js")
     ]))
 
+(def bfa-page
+  (html5
+   [:head
+    [:meta {:charset "utf-8"}]
+    (include-css "/css/screen.css")
+    ]
+   [:body
+    [:div#app
+     [:h2#change-me "Changing..."]]
+    (include-js "/js/compiled/bfa.js")
+    ]))
+
 (defroutes app-routes
-  (GET "/" [] main-page)
-  (resources "/" {:root "im/public"}))
+  (GET "/" [] #(:page-template %))
+  ;; FIXME (resources "/" #(:resources-template %))
+  (resources "/" {:root "im/public"})
+  (resources "/" {:root "bfa/public"}))
+
+
+(defn get-page-template [server-name]
+  (let [page-for-name {"bfa.local" bfa-page
+                       "misc.local" im-page}]
+    (get page-for-name server-name)))
+
+(defn get-res-template [server-name]
+  (let [res-for-name {"bfa.local" {:root "bfa/public"}
+                      "misc.local" {:root "im/public"}]
+    (get res-for-name server-name)))
+
+(defn wrap-serve-template
+  [handler]
+  (fn [request]
+    (let [server-name (:server-name request)
+          page-template (get-page-template server-name)
+          res-template (get-res-template server-name)]
+      (handler (assoc request
+                      :page-template page-template
+                      :resources-template res-template)))))
 
 (def app
   (routes
    (-> app-routes
        (wrap-defaults site-defaults)
-       wrap-reload)))
+       wrap-reload
+       wrap-serve-template)))
--- src/clj/im/server.clj
+++ src/clj/im/server.clj
@@ -1,11 +1,8 @@
 (ns im.server
   (:use [org.httpkit.server :only [run-server]])
-  (:require [im.handler]
-            [bfa.handler]))
+  (:require [im.handler]))
 
 (defn -main [& args]
   (let [port (Integer. (nth args 0 "3000"))]
-    (case port
-      3000 (run-server im.handler/app {:port port})
-      3001 (run-server bfa.handler/app {:port port}))
+    (run-server im.handler/app {:port port})
     (println "serving site on port" port)))
