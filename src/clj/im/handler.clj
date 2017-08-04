36bbe90f935ca59b27a949240fe648e506f74a69
Manuel Quiñones <manuel.por.aca@gmail.com>
2017-08-04 08:46:22 -0300

    solve resources issue

--- src/clj/im/handler.clj
+++ src/clj/im/handler.clj
@@ -2,6 +2,7 @@
   (:require [compojure.core :refer [GET routes defroutes]]
             [compojure.route :refer [resources]]
             [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.file :refer [wrap-file]]
             [ring.middleware.defaults :refer [wrap-defaults
                                               site-defaults]]
             [hiccup.page :refer [include-js include-css html5]]))
@@ -31,35 +32,34 @@
     ]))
 
 (defroutes app-routes
-  (GET "/" [] #(:page-template %))
-  ;; FIXME (resources "/" #(:resources-template %))
-  (resources "/" {:root "im/public"})
-  (resources "/" {:root "bfa/public"}))
-
+  (GET "/" [] #(:page-template %)))
 
 (defn get-page-template [server-name]
   (let [page-for-name {"bfa.local" bfa-page
                        "misc.local" im-page}]
     (get page-for-name server-name)))
 
-(defn get-res-template [server-name]
-  (let [res-for-name {"bfa.local" {:root "bfa/public"}
-                      "misc.local" {:root "im/public"}]
+(defn get-resource-template [server-name]
+  (let [res-for-name {"bfa.local" "resources/bfa/public"
+                      "misc.local" "resources/im/public"}]
     (get res-for-name server-name)))
 
-(defn wrap-serve-template
-  [handler]
+(defn wrap-serve-template [handler]
   (fn [request]
     (let [server-name (:server-name request)
-          page-template (get-page-template server-name)
-          res-template (get-res-template server-name)]
-      (handler (assoc request
-                      :page-template page-template
-                      :resources-template res-template)))))
+          template (get-page-template server-name)]
+      (handler (assoc request :page-template template)))))
+
+(defn wrap-file-template [handler]
+  (fn [request]
+    (let [server-name (:server-name request)
+          res-template (get-resource-template server-name)]
+      ((wrap-file handler res-template) request))))
 
 (def app
   (routes
    (-> app-routes
        (wrap-defaults site-defaults)
        wrap-reload
-       wrap-serve-template)))
+       wrap-serve-template
+       wrap-file-template)))
